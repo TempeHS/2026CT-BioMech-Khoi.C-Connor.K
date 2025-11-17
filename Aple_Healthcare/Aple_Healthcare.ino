a5eed3a2fe73c1999ba338f655d03af33de46ce6
KhoiC <khoi.cao1@education.nsw.gov.au>
2025-11-17 12:14:20 +1100

    OLED thing1

--- Aple_Healthcare/Aple_Healthcare.ino
+++ Aple_Healthcare/Aple_Healthcare.ino
@@ -1,4 +1,13 @@
+#include <math.h>
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C OLED(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
+
 void setup() {
+  Serial.begin(9600);
+  
   heartRate_setup();
   OLED_setup();
   speaker_setup();
