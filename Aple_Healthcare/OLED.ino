9a9518700e6944894d38e753b427ffcbbfcb4259
KhoiC <khoi.cao1@education.nsw.gov.au>
2025-11-19 11:42:45 +1100

    OLED 2

--- Aple_Healthcare/OLED.ino
+++ Aple_Healthcare/OLED.ino
@@ -5,26 +5,26 @@ void OLED_setup() {
 
 void OLED_loop() {
   String rateString = String(rate);
+  
   String infoString;
 
   OLED.clearBuffer();
-  if (rate >= 40 && rate < 130) {
+  if (rate >= 60 && rate < 130) {
     OLED.drawTriangle(64,16, 24,48, 104,48);
-    OLED.setDrawColor(0);
-    infoString = "BPM is moderate";
+    infoString = ("BPM is moderate");
   }
   if (rate >= 130) {
     OLED.drawEllipse(64, 32, 16, 16, U8G2_DRAW_ALL);
-    OLED.setDrawColor(0);
-    infoString = "BPM is NOT chill";
+    infoString = ("BPM is NOT chill");
   }
-  if (rate < 40) { 
-    OLED.setDrawColor(0);
-    infoString = "BPM is chill";
+  if (rate < 60) {
+    infoString = ("BPM is chill");
   }
+
+  Serial.println(infoString);
   
+  OLED.drawStr(32, 56, infoString.c_str());
   OLED.drawStr(64, 32, rateString.c_str());
-  OLED.drawStr(64, 56, infoString.c_str());
-  
   OLED.nextPage();
+  
 }
