993834d397d83cbb99af21d83e61c9a34a54f447
KhoiC <khoi.cao1@education.nsw.gov.au>
2025-11-24 09:36:15 +1100

    Update Speaker.ino

--- Aple_Healthcare/Speaker.ino
+++ Aple_Healthcare/Speaker.ino
@@ -1,20 +1,19 @@
 #define SPEAKER 6
 
-void speaker_setup(){
-  pinMode(SPEAKER,OUTPUT);
-
+void speaker_setup() {
+  pinMode(SPEAKER, OUTPUT);
 }
 void speaker() {
-  if(rate >= 70){
+  if (rate >= 70) {
     tone(SPEAKER, 523, 200);  // C5
     delay(250);
     tone(SPEAKER, 659, 200);  // E5
     delay(250);
     tone(SPEAKER, 784, 200);  // G5
     delay(250);
-    tone(SPEAKER, 1047, 400); // C6
+    tone(SPEAKER, 1047, 400);  // C6
     delay(500);
     noTone(SPEAKER);
   }
-  digitalWrite(SPEAKER,LOW);
+  digitalWrite(SPEAKER, LOW);
 }
